be0bb6cb5d72a1814aa476eed9b1636065eda343
Jonathan Keith <jk@jonkeith.com>
2013-02-20 20:19:55 -0600

    refactoring for readability, add link to change url

--- Bot/Formatters/BuildStatusFormatter.cs
+++ Bot/Formatters/BuildStatusFormatter.cs
@@ -9,32 +9,68 @@ namespace Bot.Formatters
 {
     public class BuildStatusFormatter : IIrcMessageFormatter<SyndicationItem>
     {
+        private SyndicationItem build; 
+
         public IEnumerable<string> Format(SyndicationItem build)
         {
-            var messages = new List<string>();
+            this.build = build;
 
+            var heading = GetHeaderText();
+            var tests = GetTestMessage();
+            var changes = GetChangesUrl();
+
+            return new List<string> {
+                heading, 
+                tests,
+                changes
+            };
+        }
+
+        private string GetHeaderText()
+        {
             var title = build.Title.Text;
-            var summary = build.Summary.Text;
             var publishDate = build.PublishDate.ToLocalTime().ToString("T");
 
-            var startPosition = summary.IndexOf("<strong>") + "<strong>".Length;
+            return string.Format("{0} @ {1}", title, publishDate);
+        }
+
+        private string GetTestMessage()
+        {
+
+            var summary = build.Summary.Text;
+            var startTag = "<strong>";
+            var startPosition = summary.IndexOf(startTag) + startTag.Length;
+
             var testSummary = summary.Substring(
-                    startPosition,
-                    summary.IndexOf("</strong>") - startPosition
+                startPosition,
+                summary.IndexOf("</strong>") - startPosition
             );
 
             var numberOfTestsText = testSummary.Split(' ')[2];
             var numberOfTests = int.Parse(numberOfTestsText.Substring(0, numberOfTestsText.Length - 1));
             var testsLeftToParty = 1337 - numberOfTests;
             testSummary += string.Format(
-                ", {0} left to partay!", 
+                ", {0} left to partay!",
                 testsLeftToParty
-            );
+                );
 
-            messages.Add(string.Format("{0} @ {1}", title, publishDate));
-            messages.Add(testSummary);
+            return testSummary;
+        }
 
-            return messages;
+        private string GetChangesUrl()
+        {
+            var link = build.Links.First();
+            if (link != null)
+            {
+                var baseUrl = link.Uri.ToString();
+                var url = baseUrl + "&tab=buildChangesDiv";
+                return string.Format(
+                    "changes: {0}",
+                    url
+                );
+            }
+
+            return string.Empty;
         }
     }
 }
