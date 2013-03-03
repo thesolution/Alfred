944da239bc2a0f873cbee9b220211aaff7de57c2
Kijana Woodard <github@wyldeye.com>
2013-03-03 13:03:22 -0600

    add roll-dice command

---
+++ Bot/Commands/DiceCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Bot.Commands.Attributes;
+
+namespace Bot.Commands
+{
+	[IrcCommand("roll-dice")]
+	public class DiceCommand : IrcCommandProcessor
+	{
+		public override void Process(IrcCommand command)
+		{
+			base.Process(command);
+
+			const int dice = 2;
+			const int sides = 6;
+
+			var random = new Random();
+			var roll = random.Next(1, dice * sides);
+			var result = "I rolled a " + roll;
+			
+			SendMessage(result);
+		}
+	}
+}
