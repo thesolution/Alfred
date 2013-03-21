2917fd2bfbe229a6d3e903c76769e1918c05bf40
Jonathan Keith <jk@jonkeith.com>
2013-03-21 11:55:59 -0500

    aws elb repair support commands

---
+++ Bot/Commands/AWS/AwsElbRepairHelpCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bot.Commands.Attributes;
+
+namespace Bot.Commands.AWS
+{
+    [AwsElbRepairCommand("help")]
+    public class AwsElbRepairHelpCommand : IrcCommandProcessor
+    {
+        public override void Process(IrcCommand command)
+        {
+            base.Process(command);
+
+            SendMessage(
+                "It's easy! Just type this: aws elb repair <load balancer name>"
+            );
+        }
+    }
+}
---
+++ Bot/Commands/Attributes/AwsElbRepairCommandAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Bot.Commands.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class AwsElbRepairCommandAttribute : IrcCommandAttribute
+    {
+        public AwsElbRepairCommandAttribute(string name) : base(name) { }
+    }
+}
