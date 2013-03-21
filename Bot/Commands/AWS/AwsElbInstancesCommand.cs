c6b2766882257cbce7b1ee9ad8bcded16223e4dc
Jonathan Keith <jk@jonkeith.com>
2013-03-21 11:55:18 -0500

    new command to list elb instances (via notice)

---
+++ Bot/Commands/AWS/AwsElbInstancesCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Commands.Attributes;
+using Bot.Infrastructure.AWS;
+
+namespace Bot.Commands.AWS
+{
+    [AwsElbCommand("instances")]
+    public class AwsElbInstancesCommand : IrcCommandProcessor
+    {
+        public override void Process(IrcCommand command)
+        {
+            base.Process(command);
+
+            if (HandleNoParameters("I need the load balancer name.", null))
+                return;
+
+            var loadBalancerName = command.Parameters[0];
+            var elb = new ELB();
+            var ec2 = new EC2();
+            var instances = elb.Instances(loadBalancerName);
+            var descriptions = ec2.InstanceDescriptions(instances.Select(i => i.InstanceId));
+
+            foreach (var description in descriptions)
+            {
+                SendNotice(
+                    string.Format(
+                        @"id: {0} / ip: {1} / state: {2} / type: {3} / ami: {4} / dns: {5}",
+                        description.InstanceId,
+                        description.IpAddress,
+                        description.InstanceState.Name,
+                        description.InstanceType,
+                        description.ImageId,
+                        description.PublicDnsName
+                    )
+                );
+            }
+        }
+    }
+}
