e91c0bcd4abfa604b640ff9df6e559591f88d8e0
Jonathan Keith <jk@jonkeith.com>
2013-03-22 13:10:26 -0500

    add user login command

---
+++ Bot/Commands/User/UserLoginHelpCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Commands.User
+{
+    public class UserLoginHelpCommand : IrcCommandProcessor
+    {
+        public override void Process(IrcCommand command)
+        {
+            base.Process(command);
+
+            SendPrivateMessage("usage: login <username> <password>");
+        }
+
+    }
+}
